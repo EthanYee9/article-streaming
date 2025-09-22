bb55657fc720542dc72ca6e2fad16c44b74b524d
EthanYee9 <ethanyee9@gmail.com>
2025-09-22 14:48:25 +0100

    adding api call function and inputs functions

--- src/extract.py
+++ src/extract.py
@@ -1,19 +1,34 @@
 import requests 
+from datetime import datetime
+
+URL = "https://content.guardianapis.com/search"
 
 def inputs():
     search_dict = {
-        "search_term": None, 
-        "date_from": None, 
-        "message_broker_id": None 
+        "q": None, 
+        "from-date": None,
+        "api-key": "26a04467-0032-4c68-b7b2-93c78e09e549"
     }
     
     print("Please enter a search term: ...")
-    search_dict["search_term"] =  input().strip()
-    print("Please enter a search from date (dd-mm-yy) or leave blank to search all years: ...")
-    search_dict["date_from"] =  input().strip()
+    search_dict["q"] =  input().strip()
+
+    print("Please enter a search from date (yyyy/mm/dd) or leave blank to search all years: ...")
+    date_str =  input().strip() 
+    if date_str:
+        search_dict["from-date"] = datetime.strptime(date_str, '%Y/%m/%d').date()
+    else:
+        search_dict["from-date"] = None
+    
     print("Please enter message broker ID: ...")
-    search_dict["message_broker_id"] =  input().strip()
+    message_broker_id =  input().strip()
+    print(search_dict["from-date"])
+    return search_dict, message_broker_id
 
-    return search_dict
+def guardian_api_call():
+    search_dict, message_broker_id = inputs()
+    response = requests.get(URL, params=search_dict)
+    data = response.json()
+    print(data)
 
-inputs()
+guardian_api_call()
