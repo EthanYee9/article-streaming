afd37de14423d18edaeff4cd8204ee681e7b0ac9
EthanYee9 <ethanyee9@gmail.com>
2025-09-22 18:59:05 +0100

    adding docstrings and removing input logic

--- src/extract.py
+++ src/extract.py
@@ -3,35 +3,42 @@ from datetime import datetime
 import os 
 import dotenv 
 
-URL = "https://content.guardianapis.com/search"
+dotenv.load_dotenv()
 
-def inputs():
-    dotenv.load_dotenv()
+def get_search_params(search_term: str, message_broker_id: str, from_date: str = None):
+    """
+    Create a search dictionary and broker ID for Guardian API.
+
+    Parameters:
+        search_term (str): The term to search for.
+        from_date (str): Optional start date in 'yyyy/mm/dd' format.
+        message_broker_id (str): The message broker ID to publish to.
+
+    Returns:
+        tuple: (search_dict, message_broker_id)
+    """
+        
     search_dict = {
-        "q": None, 
+        "q": search_term.strip(), 
         "from-date": None,
         "api-key": f"{os.environ["api-key"]}"
     }
-    
-    print("Please enter a search term: ...")
-    search_dict["q"] =  input().strip()
 
-    print("Please enter a search from date (yyyy/mm/dd) or leave blank to search all years: ...")
-    date_str =  input().strip() 
-    if date_str:
-        search_dict["from-date"] = datetime.strptime(date_str, '%Y/%m/%d').date()
-    else:
-        search_dict["from-date"] = None
+    if from_date:
+        search_dict["from-date"] = datetime.strptime(from_date, '%Y/%m/%d').date()
     
-    print("Please enter message broker ID: ...")
-    message_broker_id =  input().strip()
-    print(search_dict["api-key"])
     return search_dict, message_broker_id
 
-def guardian_api_call():
-    search_dict, message_broker_id = inputs()
+def guardian_api_call(search_term: str,  message_broker_id: str, from_date: str = None):
+    """
+    Call the Guardian API with search parameters and return the JSON data.
+    """
+
+    URL = "https://content.guardianapis.com/search"
+    search_dict, message_broker_id = get_search_params(search_term, message_broker_id, from_date)
     response = requests.get(URL, params=search_dict)
     data = response.json()
     print(data)
+    return data 
 
-guardian_api_call()
+guardian_api_call("Machine learning", "Guardian", "2025/06/01")
