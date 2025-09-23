76e4cbf455fe9b98531aba3e46cbc39b06925eff
EthanYee9 <ethanyee9@gmail.com>
2025-09-23 16:45:32 +0100

    altering extract functions

--- src/extract.py
+++ src/extract.py
@@ -2,10 +2,11 @@ import requests
 from datetime import datetime
 import os 
 import dotenv 
+import json
 
 dotenv.load_dotenv()
 
-def get_search_params(search_term: str, message_broker_id: str, from_date: str = None):
+def get_search_params(search_term: str, from_date: str = None):
     """
     Create a search dictionary and broker ID for Guardian API.
 
@@ -15,13 +16,12 @@ def get_search_params(search_term: str, message_broker_id: str, from_date: str =
         message_broker_id (str): The message broker ID to publish to.
 
     Returns:
-        tuple: (search_dict, message_broker_id)
+        dictionary: search_dict
     """
         
     search_dict = {
         "q": search_term.strip(), 
         "from-date": None,
-        "order-by": "newest",
         "api-key": f"{os.environ["api-key"]}"
     }
 
@@ -31,18 +31,33 @@ def get_search_params(search_term: str, message_broker_id: str, from_date: str =
         except ValueError as e:
             raise ValueError("from_date must be in the format of yyyy/mm/dd, e.g. 2001/06/17")
     
-    return search_dict, message_broker_id
+    return search_dict
 
-def guardian_api_call(search_term: str,  message_broker_id: str, from_date: str = None):
+
+def guardian_api_call(search_dict: dict):
     """
     Call the Guardian API with search parameters and return the JSON data.
     """
 
     URL = "https://content.guardianapis.com/search"
-    search_dict, message_broker_id = get_search_params(search_term, message_broker_id, from_date)
     response = requests.get(URL, params=search_dict)
     data = response.json()
-    print(data)
-    return data 
+    print(type(data["response"]["results"][0]["webPublicationDate"]))
+    return data
 
-get_search_params("Machine learning", "Guardian", "2025-06-01")
+    # with open("sample.json", "w") as f:
+    #     json.dump(data, f, indent=2)
+    # return data 
+
+def main(search_term: str, message_broker_id: str, from_date: str = None):
+    search_dict = get_search_params(search_term, from_date)
+    article_data = guardian_api_call(search_dict)
+    return article_data, message_broker_id
+
+
+# guardian_api_call({
+    #     "q": "Machine learning", 
+    #     "from-date": None,
+    #     "api-key": "test"
+    # })
+
