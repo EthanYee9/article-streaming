3d1fd516c25dfe2c8243c5224be72fc6e5d41520
EthanYee9 <ethanyee9@gmail.com>
2025-09-24 11:06:18 +0100

    adding controller function to transform

--- src/transform.py
+++ src/transform.py
@@ -11,18 +11,24 @@ def order_by_newest(api_data: dict):
 def extract_relevant_fields(api_data: dict):
     articles = []
 
-    for article in api_data["response"]["results"]:
+    for article in api_data:
         article_data = {}
         article_data["webPublicationDate"] = article["webPublicationDate"]
         article_data["webTitle"] = article["webTitle"]
         article_data["webUrl"] = article["webUrl"]
         article_data["content_preview"] = article["fields"]["body"][:1000]
         articles.append(article_data)
-    print(articles)
+    
     return articles
 
+def transform_data(api_data: dict):
+    sorted_articles = order_by_newest(api_data)
+    data = extract_relevant_fields(sorted_articles)
+    with open("transform.json", "w") as f:
+        json.dump(data, f, indent=2)
+    return data 
 
 # test code 
 with open("sample.json", "r") as f:
     data = json.load(f)
-extract_relevant_fields(data)
+transform_data(data)
