0a8e8a53dda45a01bc42247fe057f38319a331c7
EthanYee9 <ethanyee9@gmail.com>
2025-10-02 11:17:05 +0100

    removing duplicate code

--- terraform/kinesis.tf
+++ terraform/kinesis.tf
@@ -6,28 +6,3 @@ resource "aws_kinesis_stream" "Guardian_content" {
     stream_mode = "ON_DEMAND"
   }
 }
-
-data "aws_iam_policy_document" "kinesis_data_policy_doc" {
-  statement {
-    effect = "Allow"
-    actions = [
-      "kinesis:DescribeStreamSummary",
-      "kinesis:ListShards",
-      "kinesis:PutRecord",
-      "kinesis:PutRecords"
-    ]
-    resources = [aws_kinesis_stream.Guardian_content.arn]
-  }
-}
-
-resource "aws_iam_policy" "kinesis_policy" {
-  name   = "kinesis_policy"
-  policy = data.aws_iam_policy_document.kinesis_data_policy_doc.json
-}
-
-# resource "aws_iam_policy_attachment" "kinesis_policy" {
-#   name       = "kinesis_policy"
-#   roles      = "article-streaming-admin" # switch to lambda iam role 
-#   policy_arn = aws_iam_policy.kinesis_policy.arn
-# }
-
