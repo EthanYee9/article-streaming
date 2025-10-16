8ecd83d4bdc0b4e102ab39fc286fd1fee85bbb0d
EthanYee9 <ethanyee9@gmail.com>
2025-10-16 11:48:41 +0100

    adding test data

---
+++ tests/data/extract_test_data.py
@@ -0,0 +1,24 @@
+def extract_fake_data():
+    return {
+        "response": {
+            "status": "ok",
+            "results": [
+                {
+                    "id": "technology/2025/sep/16/how-ai-is-undermining-learning-and-teaching-in-universities",
+                    "type": "article",
+                    "sectionId": "technology",
+                    "sectionName": "Technology",
+                    "webPublicationDate": "2025-09-16T16:22:48Z",
+                    "webTitle": "How AI is undermining learning and teaching in universities | Letter",
+                    "webUrl": "https://www.theguardian.com/technology/2025/sep/16/how-ai-is-undermining-learning-and-teaching-in-universities",
+                    "apiUrl": "https://content.guardianapis.com/technology/2025/sep/16/how-ai-is-undermining-learning-and-teaching-in-universities",
+                    "fields": {
+                        "body": '<p>In discussing generative artificial intelligence (<a href="https://www.theguardian.com/education/2025/sep/13/its-going-to-be-a-life-skill-educators-discuss-the-impact-of-ai-on-university-education">\u2018It\u2019s going to be a life skill\u2019: educators discuss the impact of AI on university education, 13 September</a>) you appear to underestimate the challenges that large language model (LLM) tools such as ChatGPT present to higher education. </p>'
+                    },
+                    "isHosted": False,
+                    "pillarId": "pillar/news",
+                    "pillarName": "News",
+                }
+            ],
+        }
+    }
